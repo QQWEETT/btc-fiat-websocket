33527f51f563ae7969af0aff63f7f5dcb9a82d5a
QQWEETT <94145619+QQWEETT@users.noreply.github.com>
2022-07-14 15:47:37 +0300

    Update btc_fiat_processor.go

--- internals/app/processors/btc_fiat_processor.go
+++ internals/app/processors/btc_fiat_processor.go
@@ -10,10 +10,12 @@ type BTCFiatProcessor struct {
 	storage *db.BTCFiatStorage
 }
 
-var Chan3 = make(chan []models.BTCFiat)
-var Chan4 = make(chan models.Fiat)
 var Chan = make(chan float64)
 var Chan2 = make(chan bool, 1)
+var Chan3 = make(chan []models.BTCFiat)
+var Chan4 = make(chan models.Fiat)
+
+
 
 func NewBTCFiatProcessor(storage *db.BTCFiatStorage) *BTCFiatProcessor {
 	processor := new(BTCFiatProcessor)
